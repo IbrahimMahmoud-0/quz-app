504c19c2e947d12bc88f87753e048bcb7c5a2993
IbrahimMahmoud-0 <0ibrahimmahmoud@gmail.com>
2024-02-14 14:09:41 +0200

    Test

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const body = document.querySelector('body');
-const countSpan = document.querySelector(' .count span');
+const body = document.querySelector("body");
+const countSpan = document.querySelector(" .count span");
 
-const spansContianer = document.querySelector('.spans');
+const spansContianer = document.querySelector(".spans");
 
-const quizArea = document.querySelector('.quiz-area');
+const quizArea = document.querySelector(".quiz-area");
 
-const answerArea = document.querySelector('.answers-area');
+const answerArea = document.querySelector(".answers-area");
 
-const submitBtn = document.querySelector('.submit-button');
+const submitBtn = document.querySelector(".submit-button");
 
-const resultsContianer = document.querySelector('.results');
+const resultsContianer = document.querySelector(".results");
 
-const quizApp = document.querySelector('.quiz-app');
+const quizApp = document.querySelector(".quiz-app");
 
-const quizMin = document.querySelector('.minutes');
-const quizSec = document.querySelector('.seconds');
+const quizMin = document.querySelector(".minutes");
+const quizSec = document.querySelector(".seconds");
 
-const countDownContianer = document.querySelector('.countdown');
+const countDownContianer = document.querySelector(".countdown");
 let currIndex = 0;
 let rightAnswers = 0;
 let countDownInterval;
@@ -24,8 +24,8 @@ let timeOFQuiz = 4; //minutes
 
 const getQuestions = async function () {
   try {
-    const myRequest = await fetch('html_questions.json');
-    if (!myRequest.ok) throw new Error('Unable to find the path! ');
+    const myRequest = await fetch("html_questions.json");
+    if (!myRequest.ok) throw new Error("Unable to find the path! ");
     const data = await myRequest.json();
     let questionsCount = data.length;
     createBullets(questionsCount);
@@ -34,7 +34,7 @@ const getQuestions = async function () {
 
     countDown(timeOFQuiz, questionsCount);
 
-    submitBtn.addEventListener('click', function (e) {
+    submitBtn.addEventListener("click", function (e) {
       e.preventDefault();
       if (currIndex === questionsCount) return;
 
@@ -42,7 +42,7 @@ const getQuestions = async function () {
       currIndex++;
 
       checkRightAnswer(rightAnswer, questionsCount);
-      quizArea.innerHTML = answerArea.innerHTML = '';
+      quizArea.innerHTML = answerArea.innerHTML = "";
       addQuestionData(data[currIndex], questionsCount);
       //handling bullets
       // spansContianer.innerHTML = '';
@@ -65,36 +65,36 @@ function createBullets(num) {
 
 function createSpans(num, currIndex) {
   for (let i = 0; i < num; i++) {
-    const span = document.createElement('span');
+    const span = document.createElement("span");
     if (i === currIndex) {
-      span.className = 'on';
+      span.className = "on";
     }
-    spansContianer.insertAdjacentElement('beforeend', span);
+    spansContianer.insertAdjacentElement("beforeend", span);
   }
 }
 
 function addQuestionData(obj, count) {
   if (currIndex < count) {
-    let questionTitle = document.createElement('h2');
+    let questionTitle = document.createElement("h2");
 
     let questionText = document.createTextNode(obj.title);
     questionTitle.appendChild(questionText);
     quizArea.appendChild(questionTitle);
 
     for (let i = 0; i < 4; i++) {
-      let mainDiv = document.createElement('div');
-      mainDiv.className = 'answer';
+      let mainDiv = document.createElement("div");
+      mainDiv.className = "answer";
 
-      let radioInput = document.createElement('input');
-      radioInput.name = 'question';
-      radioInput.type = 'radio';
+      let radioInput = document.createElement("input");
+      radioInput.name = "question";
+      radioInput.type = "radio";
       radioInput.id = `answer_${i + 1}`;
       radioInput.dataset.answer = obj[`answer_${i + 1}`];
       if (i === 0) {
         radioInput.checked = true;
       }
 
-      let theLabel = document.createElement('label');
+      let theLabel = document.createElement("label");
       theLabel.htmlFor = `answer_${i + 1}`;
       let theLabelText = document.createTextNode(obj[`answer_${i + 1}`]);
 
@@ -110,19 +110,19 @@ function addQuestionData(obj, count) {
 }
 
 function checkRightAnswer(correctAns, qCount) {
-  let answers = document.getElementsByName('question');
+  let answers = document.getElementsByName("question");
   let [chosenAnswer] = [...answers]
-    .filter(ans => ans.checked)
-    .map(ans => ans.dataset.answer);
+    .filter((ans) => ans.checked)
+    .map((ans) => ans.dataset.answer);
   // console.log(chosenAnswer);
   if (chosenAnswer === correctAns) {
     rightAnswers++;
   }
 }
 function handleBullets() {
-  const spans = spansContianer.querySelectorAll('span');
+  const spans = spansContianer.querySelectorAll("span");
   spans.forEach((span, i) => {
-    i === currIndex ? (span.className = 'on') : '';
+    i === currIndex ? (span.className = "on") : "";
   });
 }
 
@@ -132,19 +132,19 @@ function showResults(count) {
     quizArea.remove();
     answerArea.remove();
     submitBtn.remove();
-    spansContianer.closest('.bullets').remove();
+    spansContianer.closest(".bullets").remove();
 
     if (rightAnswers > count / 2 && rightAnswers < count) {
       results = `<span class="good">Good</span>,${rightAnswers} From ${count} Is Good.`;
-      body.style.backgroundColor = 'cyan';
+      body.style.backgroundColor = "cyan";
     } else if (rightAnswers === count) {
       results = `<span class="perfect">Perfect</span>,All Answers Are Correct! .`;
-      body.style.backgroundColor = 'royalblue';
+      body.style.backgroundColor = "royalblue";
     } else {
       results = `<span class="bad">Bad</span>,You Faild successfully!! .`;
-      body.style.backgroundColor = 'red';
+      body.style.backgroundColor = "red";
     }
-    body.classList.add('flex');
+    body.classList.add("flex");
     resultsContianer.innerHTML = results;
   }
 }
@@ -153,7 +153,7 @@ function countDown(duration, count) {
   if (currIndex < count) {
     let time = duration * 60;
     countDownInterval = setInterval(function () {
-      const minutes = String(Math.trunc(time / 60)).padStart(2, '0');
+      const minutes = String(Math.trunc(time / 60)).padStart(2, "0");
       // if time is 100 and the sec is 60 (100-60)= 40 sec the remain
       const sec = String(Math.trunc(time % 60)).padStart(2, 0);
       // console.log(minutes);
@@ -168,3 +168,5 @@ function countDown(duration, count) {
   }
 }
 // countDown();
+
+console.log("Test");
